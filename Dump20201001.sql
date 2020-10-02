c3dcc37e1500c06706de9118feb49ba4ba919ad8
Maksim <maksim.kopylov.x5@gmail.com>
2020-10-02 09:39:39 +0300

    Home Work SQL

---
+++ Dump20201001.sql
@@ -0,0 +1,170 @@
+CREATE DATABASE  IF NOT EXISTS `peace` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `peace`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: peace
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gorod`
+--
+
+DROP TABLE IF EXISTS `gorod`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gorod` (
+  `id_gorod` int NOT NULL,
+  `name_gorod` varchar(45) DEFAULT NULL,
+  `chislenost` int DEFAULT NULL,
+  `ploshad` int DEFAULT NULL,
+  PRIMARY KEY (`id_gorod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gorod`
+--
+
+LOCK TABLES `gorod` WRITE;
+/*!40000 ALTER TABLE `gorod` DISABLE KEYS */;
+INSERT INTO `gorod` VALUES (1,'Москва',11000,200),(2,'Колифорния',12000,100),(3,'Пекин',1000000,10);
+/*!40000 ALTER TABLE `gorod` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oblast`
+--
+
+DROP TABLE IF EXISTS `oblast`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `oblast` (
+  `id_oblast` int NOT NULL,
+  `name_oblast` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_oblast`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oblast`
+--
+
+LOCK TABLES `oblast` WRITE;
+/*!40000 ALTER TABLE `oblast` DISABLE KEYS */;
+INSERT INTO `oblast` VALUES (1,'Московская'),(2,'Колифорнийская'),(3,'Пекинская');
+/*!40000 ALTER TABLE `oblast` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `raion`
+--
+
+DROP TABLE IF EXISTS `raion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `raion` (
+  `id_raion` int NOT NULL,
+  `name_raion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_raion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `raion`
+--
+
+LOCK TABLES `raion` WRITE;
+/*!40000 ALTER TABLE `raion` DISABLE KEYS */;
+INSERT INTO `raion` VALUES (1,'Северный '),(2,'Южный'),(3,'Нет Района');
+/*!40000 ALTER TABLE `raion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `strana`
+--
+
+DROP TABLE IF EXISTS `strana`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `strana` (
+  `id_strana` int NOT NULL,
+  `name_strana` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_strana`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `strana`
+--
+
+LOCK TABLES `strana` WRITE;
+/*!40000 ALTER TABLE `strana` DISABLE KEYS */;
+INSERT INTO `strana` VALUES (1,'Россия'),(2,'США'),(3,'Китай');
+/*!40000 ALTER TABLE `strana` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `world`
+--
+
+DROP TABLE IF EXISTS `world`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `world` (
+  `id` int NOT NULL,
+  `id_strana` int DEFAULT NULL,
+  `id_oblast` int DEFAULT NULL,
+  `id_raion` int DEFAULT NULL,
+  `id_gorod` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Gorod_idx` (`id_gorod`),
+  KEY `FK_Oblast_idx` (`id_oblast`),
+  KEY `FK_Raion_idx` (`id_raion`),
+  KEY `FK_Strana_idx` (`id_strana`),
+  CONSTRAINT `FK_Gorod` FOREIGN KEY (`id_gorod`) REFERENCES `gorod` (`id_gorod`),
+  CONSTRAINT `FK_Oblast` FOREIGN KEY (`id_oblast`) REFERENCES `oblast` (`id_oblast`),
+  CONSTRAINT `FK_Raion` FOREIGN KEY (`id_raion`) REFERENCES `raion` (`id_raion`),
+  CONSTRAINT `FK_Strana` FOREIGN KEY (`id_strana`) REFERENCES `strana` (`id_strana`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `world`
+--
+
+LOCK TABLES `world` WRITE;
+/*!40000 ALTER TABLE `world` DISABLE KEYS */;
+INSERT INTO `world` VALUES (1,1,1,1,1),(2,2,2,2,2),(3,3,3,3,3);
+/*!40000 ALTER TABLE `world` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'peace'
+--
+
+--
+-- Dumping routines for database 'peace'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-01 11:36:12
---
+++ HomeWork.sql
@@ -0,0 +1,73 @@
+CREATE SCHEMA `peace` ;
+CREATE TABLE `peace`.`world` (
+  `id` INT NOT NULL,
+  `id_strana` INT NULL,
+  `id_oblast` INT NULL,
+  `id_raion` INT NULL,
+  `id_gorod` INT NULL,
+  PRIMARY KEY (`id`));  
+CREATE TABLE `peace`.`strana` (
+  `id_strana` INT NOT NULL,
+  `name_strana` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_strana`));
+  CREATE TABLE `peace`.`oblast` (
+  `id_oblast` INT NOT NULL,
+  `name_oblast` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_oblast`));
+  CREATE TABLE `peace`.`raion` (
+  `id_raion` INT NOT NULL,
+  `name_raion` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_raion`));
+CREATE TABLE `peace`.`gorod` (
+  `id_gorod` INT NOT NULL,
+  `name_gorod` VARCHAR(45) NULL,
+  `chislenost` INT NULL,
+  `ploshad` INT NULL,
+  PRIMARY KEY (`id_gorod`));
+  ALTER TABLE `peace`.`world` 
+ADD INDEX `FK_Gorod_idx` (`id_gorod` ASC) VISIBLE,
+ADD INDEX `FK_Oblast_idx` (`id_oblast` ASC) VISIBLE,
+ADD INDEX `FK_Raion_idx` (`id_raion` ASC) VISIBLE,
+ADD INDEX `FK_Strana_idx` (`id_strana` ASC) VISIBLE;
+;
+ALTER TABLE `peace`.`world` 
+ADD CONSTRAINT `FK_Gorod`
+  FOREIGN KEY (`id_gorod`)
+  REFERENCES `peace`.`gorod` (`id_gorod`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `FK_Oblast`
+  FOREIGN KEY (`id_oblast`)
+  REFERENCES `peace`.`oblast` (`id_oblast`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `FK_Raion`
+  FOREIGN KEY (`id_raion`)
+  REFERENCES `peace`.`raion` (`id_raion`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `FK_Strana`
+  FOREIGN KEY (`id_strana`)
+  REFERENCES `peace`.`strana` (`id_strana`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+  INSERT INTO `peace`.`gorod` (`id_gorod`, `name_gorod`, `chislenost`, `ploshad`) VALUES ('1', 'Москва', '11000', '200');
+INSERT INTO `peace`.`gorod` (`id_gorod`, `name_gorod`, `chislenost`, `ploshad`) VALUES ('2', 'Колифорния', '12000', '100');
+INSERT INTO `peace`.`oblast` (`id_oblast`, `name_oblast`) VALUES ('1', 'Московская');
+INSERT INTO `peace`.`oblast` (`id_oblast`, `name_oblast`) VALUES ('2', 'Колифорнийская');
+INSERT INTO `peace`.`raion` (`id_raion`, `name_raion`) VALUES ('1', 'Северный ');
+INSERT INTO `peace`.`raion` (`id_raion`, `name_raion`) VALUES ('2', 'Южный');
+INSERT INTO `peace`.`strana` (`id_strana`, `name_strana`) VALUES ('1', 'Россия');
+INSERT INTO `peace`.`strana` (`id_strana`, `name_strana`) VALUES ('2', 'США');
+INSERT INTO `peace`.`world` (`id`, `id_strana`, `id_oblast`, `id_raion`, `id_gorod`) VALUES ('1', '1', '1', '1', '1');
+INSERT INTO `peace`.`world` (`id`, `id_strana`, `id_oblast`, `id_raion`, `id_gorod`) VALUES ('2', '2', '2', '2', '2');
+INSERT INTO `peace`.`gorod` (`id_gorod`, `name_gorod`, `chislenost`, `ploshad`) VALUES ('3', 'Пекин', '1000000', '10');
+INSERT INTO `peace`.`oblast` (`id_oblast`, `name_oblast`) VALUES ('3', 'Пекинская');
+INSERT INTO `peace`.`raion` (`id_raion`, `name_raion`) VALUES ('3', 'Нет Района');
+INSERT INTO `peace`.`strana` (`id_strana`, `name_strana`) VALUES ('3', 'Китай');
+INSERT INTO `peace`.`world` (`id`, `id_strana`, `id_oblast`, `id_raion`, `id_gorod`) VALUES ('3', '3', '3', '3', '3');
+
+
+select `world`.`id`, `strana`.`name_strana`, `oblast`.`name_oblast`, `raion`.`name_raion`, `gorod`.`name_gorod`, `gorod`.`chislenost`, `gorod`.`ploshad` AS `ploshad` from `world` inner join `strana` on `world`.`id_strana` = `strana`.`id_strana` inner join `oblast` on `world`.`id_oblast` = `oblast`.`id_oblast` inner join `raion` on `world`.`id_raion` = `raion`.`id_raion` inner join `gorod` on `world`.`id_gorod` = `gorod`.`id_gorod`;
+
+
